de4803b8526cd89414468f1b54b996751f2033be
martarzem <marta.rzempowska@gmail.com>
2020-05-10 22:00:38 +0200

    Commit -m jako pierwszy / po amedzie

--- demo/src/main/java/com/example/demo/testingclass.java
+++ demo/src/main/java/com/example/demo/testingclass.java
@@ -4,4 +4,11 @@ public class testingclass {
     public void method1() {}
     public void method2() {}
 
+    public void method3() {}
+    public void method4() {}
+
+    public void method5() {}
+
+    public void method6() {}
+
 }
