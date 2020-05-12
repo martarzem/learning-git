99999a8877c023eae0145dfb5f2f96b2dc0a2acb
martarzem <marta.rzempowska@gmail.com>
2020-05-12 22:31:30 +0200

    Testing class fix #3

--- demo/src/main/java/com/example/demo/testingclass.java
+++ demo/src/main/java/com/example/demo/testingclass.java
@@ -1,11 +1,11 @@
 package com.example.demo;
 
 public class testingclass {
-    public void method1() {}
-    public void method2() {}
-
     public void method3() {}
     public void method4() {}
 
     public void method50() {}
+
+    public void method60()
+
 }
