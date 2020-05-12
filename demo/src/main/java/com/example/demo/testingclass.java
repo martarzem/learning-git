788dc500d27f10415c569097ab2f5347565e355c
martarzem <marta.rzempowska@gmail.com>
2020-05-12 22:07:13 +0200

    Testing class fix #2 Please enter the commit message for your changes. Lines starting

--- demo/src/main/java/com/example/demo/testingclass.java
+++ demo/src/main/java/com/example/demo/testingclass.java
@@ -4,4 +4,8 @@ public class testingclass {
     public void method1() {}
     public void method2() {}
 
+    public void method3() {}
+    public void method4() {}
+
+    public void method50() {}
 }
